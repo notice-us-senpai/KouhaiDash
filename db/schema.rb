80244940b1abc09d37181a123e11fb8f17ba8bef
Jia Yee <jiayeerawr@gmail.com>
2016-05-13 20:06:49 +0800

    Validate users
    1. username, password, email
    2. rails g migration add_index_to_users_email
       - add_index :users, :email, unique: true

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513113622) do
+ActiveRecord::Schema.define(version: 20160513120531) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20160513113622) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,71 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(
+    	username: "Jiayee", 
+    	password: "password", 
+    	email: "l-jiayee@comp.nus.edu.sg"
+  	)
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = ""
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "username should not be too short" do
+    @user.username = "z" * 3
+    assert_not @user.valid?
+  end
+
+  test "username should not be too long" do
+    @user.username = "z" * 17
+    assert_not @user.valid?
+  end
+
+  test "password should not be too short" do
+    @user.password = "z" * 7
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "z" * 60 + "@gmail.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[
+    	user@example.com 
+    	USER@foo.COM 
+    	A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn
+    ]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "usernames and email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
