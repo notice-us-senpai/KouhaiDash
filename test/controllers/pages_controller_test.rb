0571d29208746cce00a828a2cb8e9d1580f7d11f
Jia Yee <jiayeerawr@gmail.com>
2016-05-13 15:30:41 +0800

    Add dynamic page titles and tests
    1. yield(:title) and provide(:title, "")
    2. assert_select "title", "____ | KouhaiDash!"

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,31 +4,37 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | KouhaiDash!"
   end
 
   test "should get profile" do
     get :profile
     assert_response :success
+    assert_select "title", "Profile | KouhaiDash!"
   end
 
   test "should get calendar" do
     get :calendar
     assert_response :success
+    assert_select "title", "Calendar | KouhaiDash!"
   end
 
   test "should get tasks" do
     get :tasks
     assert_response :success
+    assert_select "title", "Tasks | KouhaiDash!"
   end
 
   test "should get files" do
     get :files
     assert_response :success
+    assert_select "title", "Files | KouhaiDash!"
   end
 
   test "should get contacts" do
     get :contacts
     assert_response :success
+    assert_select "title", "Contacts | KouhaiDash!"
   end
 
 end
