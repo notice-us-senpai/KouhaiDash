fa44b85b859c0fad9f949b7912b8f42f51eb7889
Jia Yee <jiayeerawr@gmail.com>
2016-05-13 21:54:46 +0800

    Create sessions controller and login
    1. New session
    2. Login URL and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users
 
   get 'register', to: 'users#new', as: 'register'
