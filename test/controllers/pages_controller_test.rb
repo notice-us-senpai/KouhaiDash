a594c812f3f25247ce8806ed2d54f69dd2ddea7b
Jia Yee <jiayeerawr@gmail.com>
2016-05-13 15:42:36 +0800

    Update page title and test for 'Home'
    Empty argument for provide

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,7 +4,7 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | KouhaiDash!"
+    assert_select "title", "KouhaiDash!"
   end
 
   test "should get profile" do
