5ec07f289720963c40971e8147c7badf2e3efcec
Jia Yee <jiayeerawr@gmail.com>
2016-05-14 21:02:08 +0800

    Create edit form for user and handle unsuccessful updates
    1. edit.html.erb
    2. link_to in _header.html.erb
    3. render 'edit'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :get_user, only: [:show, :edit]
+  before_action :get_user, only: [:show, :edit, :update]
 
   def show
   end
@@ -21,6 +21,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
   end
 
  	private
