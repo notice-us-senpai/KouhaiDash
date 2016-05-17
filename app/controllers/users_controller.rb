437b5ea8f3f6d88b70656cc63ce55d98d856ca21
Jia Yee <jiayeerawr@gmail.com>
2016-05-17 23:15:05 +0800

    Create user index
    "Community" tab of KouhaiDash!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   before_action :get_user, only: [:show, :edit, :update, 
     :correct_user]
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   
+  def index
+    @users = User.all
+  end
+
   def show
   end
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,6 +11,11 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+
   test "should redirect edit when not logged in" do
     get :edit, id: @user
     assert_not flash.empty?
