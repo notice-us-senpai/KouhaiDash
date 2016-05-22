1f5ddc7406eb92406106de42577cf90cb6c64768
Jia Yee <jiayeerawr@gmail.com>
2016-05-22 14:50:15 +0800

    Clean up migrations not used in this deployment
    A cause of error in heroku run rake db:migrate:reset

--- db/migrate/20160520050511_add_public_to_category.rb
+++
@@ -1,5 +0,0 @@
-class AddPublicToCategory < ActiveRecord::Migration
-  def change
-    add_column :categories, :public, :boolean
-  end
-end
--- db/migrate/20160520070720_add_cateorgy_id_to_task.rb
+++
@@ -1,6 +0,0 @@
-class AddCateorgyIdToTask < ActiveRecord::Migration
-  def change
-    add_column :tasks, :category_id, :integer
-  end
-  add_index :tasks, :category_id
-end
