4f6da32dae2faf65d88cdd28a1a02d1732335203
Jia Yee <jiayeerawr@gmail.com>
2016-06-05 19:57:53 +0800

    Fix Issue #5 with simple logic
    Further testing will be required in order to guarantee this functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,10 +9,15 @@ class PagesController < ApplicationController
   end
 
   def calendar
-    client = Signet::OAuth2::Client.new(access_token: session[:access_token])
-    service = Google::Apis::CalendarV3::CalendarService.new
-    service.authorization = client
-    @calendar_list = service.list_calendar_lists
+    @authorised = false
+    begin
+      client = Signet::OAuth2::Client.new(access_token: session[:access_token])
+      service = Google::Apis::CalendarV3::CalendarService.new
+      service.authorization = client
+      @calendar_list = service.list_calendar_lists
+      @authorised = true
+    rescue
+    end
   end
 
   def tasks
