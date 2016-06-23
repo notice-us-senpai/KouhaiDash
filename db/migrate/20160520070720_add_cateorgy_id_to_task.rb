5dbb374e188b6dd9782b01fabce72e4171546dd3
leeyimin <imleeyimin@gmail.com>
2016-06-23 16:29:58 +0800

    correcting migration file

--- db/migrate/20160520070720_add_cateorgy_id_to_task.rb
+++ db/migrate/20160520070720_add_cateorgy_id_to_task.rb
@@ -1,6 +1,6 @@
 class AddCateorgyIdToTask < ActiveRecord::Migration
   def change
     add_column :tasks, :category_id, :integer
+    add_index :tasks, :category_id
   end
-  add_index :tasks, :category_id
 end
